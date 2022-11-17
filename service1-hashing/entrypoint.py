72671c300eef426ebc856719da46a5903641076b
ezmamig <115274663+ezmamig@users.noreply.github.com>
2022-11-17 21:11:42 +0100

    secondCommit

--- service1-hashing/entrypoint.py
+++ service1-hashing/entrypoint.py
@@ -1,13 +1,9 @@
 import requests
 import sys
-import socket
-
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
-print(IPAddr)
 
 
-url = 'http://172.21.128.1:5000/'
+#url = 'http://172.21.128.1:5000/'
+url = 'http://service2-web-page:5000'
 message = requests.get(url).text
 myobj = {'md5': message}
 #x = requests.post(url, json = myobj)
