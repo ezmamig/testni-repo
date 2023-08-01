10d4c02e98cbe2ba538b5588bc4703fe54fbd4f3
Mamula <Igor.Mamula@avl.com>
2023-08-01 11:48:37 +0200

    long shot

--- Scripts/getSupportedOS.ps1
+++ Scripts/getSupportedOS.ps1
@@ -8,14 +8,4 @@ $jsonContent = Get-Content -Path $installCFGJsonFilePath -Raw
 $jsonObject = ConvertFrom-Json -InputObject $jsonContent
 
 # Obtain the value for the key "SupportedOS"
-$supportedOs = $jsonObject.SupportedOS -join ', '
-
-# Check if SupportedOS is empty or missing
-if (-not $supportedOs) {
-    # use Windows 10, Windows Server 22 as default supported os
-    $supportedOs = "Windows 10, Windows Server 2022"
-    Write-Output $supportedOs
-} else {
-    # Output the value
-    Write-Output $supportedOs
-}
+$supportedOs = $jsonObject.SupportedOS
