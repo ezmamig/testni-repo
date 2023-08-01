f2527eb6410fe5de8017eefe226b3a3fbc35cfde
Mamula <Igor.Mamula@avl.com>
2023-08-01 12:43:13 +0200

    handle json properly

--- Scripts/getSupportedOS.ps1
+++ Scripts/getSupportedOS.ps1
@@ -6,6 +6,11 @@ param (
 # Read the installCFG.json file
 $jsonContent = Get-Content -Path $installCFGJsonFilePath -Raw
 $jsonObject = ConvertFrom-Json -InputObject $jsonContent
+echo $jsonObject
 
 # Obtain the value for the key "SupportedOS"
-$supportedOs = $jsonObject.SupportedOS
+$supportedOs = $jsonObject.SupportedOS -join ', '
+$supportedOsJson = ConvertTo-Json $supportedOs -Compress -Depth 100 | Out-String
+echo $supportedOsJson
+
+Write-Output $supportedOsJson
